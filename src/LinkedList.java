bf712455c9cdc2743e4b9cfd9404c800b7bcb711
LamRAM <150258612+LamRAM@users.noreply.github.com>
2024-01-02 13:07:07 +0300

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,18 +1,17 @@
 
-
- public class Node {
+public class Node {
   int data;
   Node next;
 
-public Node(int d) {
+ public Node(int d) {
   data = d;
   next = null;
-    }
+  }
 
- }
+}
 
 public class LinkedList {
-    Node head;
+ Node head;
 
  /* your code here */
 
@@ -20,13 +19,13 @@ public class LinkedList {
 
 
 public class Main {
-public static void main(String[] args) {
- LinkedList list = new LinkedList();
- list.head = new Node(100);
-  Node second = new Node(200);
-  Node third = new Node(300);
-  list.head.next = second;
-  second.next = third; 
-  list.display();
-    }
+ public static void main(String[] args) {
+  LinkedList list = new LinkedList();
+  list.head = new Node(100);
+   Node second = new Node(200);
+   Node third = new Node(300);
+   list.head.next = second;
+   second.next = third; 
+   list.display();
+     }
 }
